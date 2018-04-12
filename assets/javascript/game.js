eeb20f0fec6cc66aeaf751ef43a8bbb6e2567e7b
Alex Kim <askim1019@gmail.com>
2018-04-12 00:48:25 -0500

    change html to match correct health

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,34 @@
 // Objects are the characters of the game
 var katarina = {
     name: "katarina",
-    hp: 500,
-    ap: 10,
+    hp: 150,
+    ap: 20,
     ba: 10,
-    counter: 20,
+    counter: 15,
 }
 
 var lucian = {
     name: "lucian",
-    hp: 100,
+    hp: 150,
     ap: 10,
-    ba: 5,
-    counter: 20,
+    ba: 12,
+    counter: 16,
 }
 
 var rengar = {
     name: "rengar",
-    hp: 100,
+    hp: 220,
     ap: 10,
     ba: 5,
-    counter: 20,
+    counter: 11,
 }
 
 var veigar = {
     name: "veigar",
-    hp: 100,
-    ap: 10,
-    ba: 5,
-    counter: 20,
+    hp: 110,
+    ap: 30,
+    ba: 14,
+    counter: 25,
 }
 
 var heroChosen = false;
@@ -41,6 +41,8 @@ var defeated = 0;
 function resetGame() {
     location.reload();
 }
+
+
 
 function heroStats(object) {
     hero.name = object.name;
@@ -60,14 +62,21 @@ function defenderStats(object) {
 
 $(document).ready(function(){
 
+    $("#reset").hide();
+
+    $("#reset").click(function(){
+        resetGame();
+    });
+
     $("#katarina").click(function() {
         if (!heroChosen) {
             $(this).appendTo("#hero");
             $(this).css({"border-color": "#53f442", "margin-left": "0"});
             $("#kathealth").addClass("health");
             $("#lucian, #rengar, #veigar").appendTo("#enemies");
-            $("#lucian, #rengar, #veigar").css("border-color", "#c10000");
+            $("#lucian, #rengar, #veigar").css({"border-color": "#c10000", "margin-left": "0"});
             $("#lucian, #rengar, #veigar").addClass("enemies");
+            $(".choose").hide();
             heroStats(katarina);
             heroChosen = true;
         } else if (heroChosen && !defenderChosen) {
@@ -91,8 +100,9 @@ $(document).ready(function(){
             $(this).css({"border-color": "#53f442", "margin-left": "0"});
             $("#lucianhealth").addClass("health");
             $("#katarina, #rengar, #veigar").appendTo("#enemies");
-            $("#katarina, #rengar, #veigar").css("border-color", "#c10000");
+            $("#katarina, #rengar, #veigar").css({"border-color": "#c10000", "margin-left": "0"});
             $("#katarina, #rengar, #veigar").addClass("enemies");
+            $(".choose").hide();
             heroChosen = true;
             heroStats(lucian);
         } else if (heroChosen && !defenderChosen) {
@@ -116,8 +126,9 @@ $(document).ready(function(){
             $(this).css({"border-color": "#53f442", "margin-left": "0"});
             $("#rengarhealth").addClass("health");
             $("#katarina, #lucian, #veigar").appendTo("#enemies");
-            $("#katarina, #lucian, #veigar").css("border-color", "#c10000");
+            $("#katarina, #lucian, #veigar").css({"border-color": "#c10000", "margin-left": "0"});
             $("#katarina, #lucian, #veigar").addClass("enemies");
+            $(".choose").hide();
             heroChosen = true;
             heroStats(rengar);
         } else if (heroChosen && !defenderChosen) {
@@ -141,8 +152,9 @@ $(document).ready(function(){
             $(this).css({"border-color": "#53f442", "margin-left": "0"});
             $("#veigarhealth").addClass("health");
             $("#katarina, #rengar, #lucian").appendTo("#enemies");
-            $("#katarina, #rengar, #lucian").css("border-color", "#c10000");
+            $("#katarina, #rengar, #lucian").css({"border-color": "#c10000", "margin-left": "0"});
             $("#katarina, #rengar, #lucian").addClass("enemies");
+            $(".choose").hide();
             heroChosen = true;
             heroStats(veigar);
         } else if (heroChosen && !defenderChosen) {
@@ -163,12 +175,14 @@ $(document).ready(function(){
         if (heroChosen && defenderChosen && !gameOver) {
             defender.hp -= hero.ap;
             hero.hp -= defender.counter;
-            hero.ap += hero.ba;
             $(".health").html(hero.hp);
             $(".def-health").html(defender.hp);
-            console.log(hero.hp);
-            console.log(defender.hp);
-            console.log(hero.ap);
+            $("#message").html("<p>" + hero.name + " attacked " + defender.name + " for " + hero.ap + " health </p><p style='color: red;'>" + 
+            defender.name + " counter-attacked " + hero.name + " for " + defender.counter + " health </p>")
+                         .css({"color": "blue", "font-size": "20px"})
+                         .addClass("text-center");
+            hero.ap += hero.ba;
+
 
             if (hero.hp <= 0) {
                 $("#message").html("<h2> Game Over </h2>")
@@ -176,12 +190,12 @@ $(document).ready(function(){
                              .addClass("text-center");
                 $(".hero").hide();
                 gameOver = true;
+                $("#reset").show();
             } else if (defender.hp <= 0 && hero.hp > 0 && defeated < 3) {
                 $(".defender").removeClass("defender")
                               .hide();
                 defenderChosen = false;
                 defeated++;
-                console.log(defeated);
             }
         }
 
@@ -189,6 +203,7 @@ $(document).ready(function(){
             $("#message").html("<h2> VICTORIOUS! </h2>")
                          .css("color", "green")
                          .addClass("text-center");
+            $("#reset").show();
         }
 
     });
